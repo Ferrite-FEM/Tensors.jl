669b84e76ba88334fc64d53c13914cd324d31322
Fredrik Ekre <fredrik.ekre@chalmers.se>
2016-11-15 11:54:34 +0100

    diagm with Tuple (#72)

--- src/ContMechTensors.jl
+++ src/ContMechTensors.jl
@@ -193,11 +193,11 @@ end
 # diagm
 for TensorType in (SymmetricTensor, Tensor)
     @eval begin
-        @generated function Base.diagm{order, dim, T}(Tt::Type{$(TensorType){order, dim}}, v::AbstractVector{T})
+        @generated function Base.diagm{order, dim}(Tt::Type{$(TensorType){order, dim}}, v::Union{AbstractVector, Tuple})
             if order == 2
-                f = (i,j) -> i == j ? :(v[$i]) : :($(zero(T)))
+                f = (i,j) -> i == j ? :(v[$i]) : :($(zero(eltype(v))))
             elseif order == 4
-                f = (i,j,k,l) -> i == k && j == l ? :(v[$i]) : :($(zero(T)))
+                f = (i,j,k,l) -> i == k && j == l ? :(v[$i]) : :($(zero(eltype(v))))
             else
                 throw(ArgumentError("diagm only defined for tensors of order 2 and 4"))
             end
