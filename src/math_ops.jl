774fcfeaf6544e4869559a66bbd14131d9b0e378
Fredrik Ekre <fredrik.ekre@chalmers.se>
2017-03-03 17:36:07 +0100

    Norm of Tensor{4, 3} (#11)
    * fix norm(Tensor{4, 3})
    
    * add a comment [ci skip]

--- src/math_ops.jl
+++ src/math_ops.jl
@@ -23,6 +23,9 @@ julia> norm(A)
 @inline Base.norm(v::Vec) = sqrt(dot(v, v))
 @inline Base.norm(S::SecondOrderTensor) = sqrt(dcontract(S, S))
 
+# special case for Tensor{4, 3} since it is faster than unrolling
+@inline Base.norm(S::Tensor{4, 3}) = sqrt(mapreduce(abs2, +, S))
+
 @generated function Base.norm{dim}(S::FourthOrderTensor{dim})
     idx(i,j,k,l) = compute_index(get_base(S), i, j, k, l)
     ex = Expr[]
