ffd84aba48935f897c332c37793e8cfec29806b2
Kristoffer Carlsson <kristoffer.carlsson@chalmers.se>
2017-03-23 12:29:39 +0100

    enforce doc strictness only on 64 bit (#31)

--- docs/make.jl
+++ docs/make.jl
@@ -5,7 +5,7 @@ makedocs(
     format = :html,
     sitename = "Tensors.jl",
     doctest = true,
-    strict = VERSION.minor == 6, # only strict mode on release bot
+    strict = VERSION.minor == 6 && sizeof(Int) == 8, # only strict mode on 0.6 and Int64
     pages = Any[
         "Home" => "index.md",
         "Manual" => [
