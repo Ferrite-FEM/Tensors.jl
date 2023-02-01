96f6bfa149f1b4a082f9e798ea56439733ba9a75
Fredrik Ekre <ekrefredrik@gmail.com>
2023-02-01 10:06:47 +0100

    Improve documentation for tdot and dott. (#193)

--- src/tensor_products.jl
+++ src/tensor_products.jl
@@ -350,6 +350,8 @@ end
 Compute the dot product of a symmetric second order tensor with itself.
 Return a `SymmetricTensor`.
 
+See also [`tdot`](@ref) and [`dott`](@ref).
+
 # Examples
 ```jldoctest
 julia> A = rand(SymmetricTensor{2,3})
@@ -368,9 +370,9 @@ julia> dot(A)
 @inline LinearAlgebra.dot(S::SymmetricTensor{2}) = tdot(S)
 
 """
-    tdot(::SecondOrderTensor)
+    tdot(A::SecondOrderTensor)
 
-Compute the transpose-dot product of a second order tensor with itself.
+Compute the transpose-dot product of `A` with itself, i.e. `dot(A', A)`.
 Return a `SymmetricTensor`.
 
 # Examples
@@ -391,9 +393,9 @@ julia> tdot(A)
 @inline tdot(S::SecondOrderTensor) = unsafe_symmetric(S' ⋅ S)
 
 """
-    dott(::SecondOrderTensor)
+    dott(A::SecondOrderTensor)
 
-Compute the dot-transpose product of a second order tensor with itself.
+Compute the dot-transpose product of `A` with itself, i.e. `dot(A, A')`.
 Return a `SymmetricTensor`.
 
 # Examples
