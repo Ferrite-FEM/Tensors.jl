c82f4dc87ff7a224c1d5d0749644b68bbf0c045e
Elias <elias.isak.borjesson@gmail.com>
2023-03-07 09:25:28 +0100

    add orthogonal isparallel function

--- src/Tensors.jl
+++ src/Tensors.jl
@@ -20,7 +20,7 @@ export tdot, dott, dotdot
 export hessian, gradient, curl, divergence, laplace
 export @implement_gradient
 export basevec, eᵢ
-export rotate, rotation_tensor
+export rotate, rotation_tensor, orthogonal
 export tovoigt, tovoigt!, fromvoigt, tomandel, tomandel!, frommandel
 #########
 # Types #
