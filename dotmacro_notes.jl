4e629a294f9a3c7ff899b6cfeb7ce6f8f7a564a0
Knut Andreas Meyer <knutam@gmail.com>
2023-10-25 08:54:06 +0200

    Remove duplicate notes

--- dotmacro_notes.jl
+++ dotmacro_notes.jl
@@ -1,36 +1,3 @@
-#=
-@tensorfun function dcontract(A::Tensor{2,dim}, B::Tensor{2,dim}) where dim    
-    C = A[i,j]*B[i,j]
-end
-@tensorfun function otimes(A::Tensor{2,dim}, B::Tensor{2,dim}) where dim    
-    C[i,j,k,l] = A[i,j]*B[k,l]
-end
-@tensorfun function otimes(A::Tensor{2,2}, B::Tensor{2,2})    
-    C[i,j,k,l] = A[i,j]*B[k,l]
-end
-
-Restrictions
-    * Must be `Tensor{order,dim}` or `SymmetricTensor{order,dim}` (`Vec` not permitted)
-    * 
-
-
-Output, if generic dim given
-function \$name(A::Tensor{\$Aorder,dim}, B::Tensor{\$Border,dim}) where dim 
-    if dim == 1
-        return \$expr_1d
-    elseif dim == 2
-        return \$expr_2d
-    else
-        return \$expr_3d
-    end
-end
-if a specific dim given 
-function \$name(A::Tensor{\$Aorder,dim}, B::Tensor{\$Border,dim}) where dim 
-    return \$expr
-end
-
-=#
-
 using Tensors
 import Tensors: IndSyms, find_index # new internals used here
 
