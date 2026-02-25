85c8d313a952a59075b62c2f648418357c71ed72
Knut Andreas <knutam@gmail.com>
2026-02-25 13:38:06 +0100

    Full support for regular_if_possible

--- src/mixed_tensors.jl
+++ src/mixed_tensors.jl
@@ -5,8 +5,14 @@ isregular(::Type{<:MixedTensor{2, dims}}) where {dims} = dims[1] === dims[2]
 isregular(::Type{<:MixedTensor{3, dims}}) where {dims} = dims[1] === dims[2] === dims[3]
 isregular(::Type{<:MixedTensor{4, dims}}) where {dims} = dims[1] === dims[2] === dims[3] === dims[4]
 
+function regular_if_possible(::Type{TT}) where {order, dims, TT <: MixedTensor{order, dims}}
+    return isregular(TT) ? Tensor{order, dims[1]} : TT
+end
 function regular_if_possible(::Type{TT}) where {order, dims, T, TT <: MixedTensor{order, dims, T}}
     return isregular(TT) ? Tensor{order, dims[1], T} : TT
+end
+function regular_if_possible(::Type{TT}) where {order, dims, T, M, TT <: MixedTensor{order, dims, T, M}}
+    return isregular(TT) ? Tensor{order, dims[1], T, M} : TT
 end
 function regular_if_possible(t::TT) where {order, dims, TT <: MixedTensor{order, dims}}
     return isregular(TT) ? Tensor{order, dims[1]}(get_data(t)) : t
