472d0e41700457e30318233c498f76de4554b00c
Knut Andreas Meyer <knutam@gmail.com>
2026-05-31 12:05:24 +0200

    Revert debug changes

--- test/test_ops.jl
+++ test/test_ops.jl
@@ -16,11 +16,6 @@ A = rand(Tensor{2, dim, T})
 B = rand(Tensor{2, dim, T})
 a = rand(Tensor{1, dim, T})
 b = rand(Tensor{1, dim, T})
-if !(dott(A) ≈ dot(A, transpose(A)))
-    @show A
-    @show dott(A)
-    @show dot(A, transpose(A))
-end
 
 AA_sym = rand(SymmetricTensor{4, dim, T})
 BB_sym = rand(SymmetricTensor{4, dim, T})
